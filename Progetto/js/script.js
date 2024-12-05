10049102452cc695b33044f630f0b256eee48fcc
melania17111 <melaniamoras02@gmail.com>
2024-12-05 18:15:08 +0100

    script delle info

--- Progetto/js/script.js
+++ Progetto/js/script.js
@@ -1,3 +1,27 @@
 function recordinfo() {
-  document.getElementById("help-window").innerHTML = "record!";
+  document.getElementById("help-window").innerHTML = "This is the record button! Press it to start and stop the recording of your sounds!";
+}
+function playinfo() {
+  document.getElementById("help-window").innerHTML = "This is the play button! Press it to listen to your recording with the effects you chose!";
+}
+function stopinfo() {
+  document.getElementById("help-window").innerHTML = "This is the stop button! Press it to stop the replay of your recording!";
+}
+function settingsinfo() {
+  document.getElementById("help-window").innerHTML = "This is the settings button! Press it to access the setting menu and choose your audio input!";
+}
+function exportinfo() {
+  document.getElementById("help-window").innerHTML = "This is the export button! Press it to export the sounds you recorded and share them with the world!";
+}
+function reverbinfo() {
+  document.getElementById("help-window").innerHTML = "This is the reverb button! Press it to add a reverb to your sound! Double click on it to access the effect’s specific settings!";
+}
+function delayinfo() {
+  document.getElementById("help-window").innerHTML = "This is the delay button! Press it to add a delay to your sound! Double click on it to access the effect’s specific settings!";
+}
+function saturatorinfo() {
+  document.getElementById("help-window").innerHTML = "This is the saturator button! Press it to add saturation to your sound! Double click on it to access the effect’s specific settings!";
+}
+function lfoinfo() {
+  document.getElementById("help-window").innerHTML = "This is the LFO button! Press it to add an LFO to your sound! Double click on it to access the effect’s specific settings!";
 }
